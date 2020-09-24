783b17ba37d49c5582e136e4a1be29dbc4fc6cfe
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-09-24 20:17:52 +0200

    Added logging to files

--- sc3a/__init__.py
+++ sc3a/__init__.py
@@ -1,11 +1,24 @@
 import logging
+from logging import handlers
+
+formatter = logging.Formatter((
+    '%(asctime)s '
+    '- %(name)s '
+    '[%(levelname)s\t] '
+    '%(message)s'
+))
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s [%(levelname)s\t] %(message)s')
 stream_handler.setFormatter(formatter)
+
+file_handler = handlers.RotatingFileHandler('logs/out.log',
+                                            maxBytes=100_000,
+                                            backupCount=5)
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
 
 log = logging.getLogger(__name__)
 log.addHandler(stream_handler)
+log.addHandler(file_handler)
 log.setLevel(logging.INFO)
