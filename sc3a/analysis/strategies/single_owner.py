22f3f104611f4e0a86248ce03536a6176c30aaa9
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-09-24 23:01:35 +0200

    Improved the single owner strategy to a more precise version by using symbols

--- sc3a/analysis/strategies/single_owner.py
+++ sc3a/analysis/strategies/single_owner.py
@@ -1,8 +1,12 @@
 import logging
-from typing import Optional, Set, List, Text
+import re
 
 from mythril.ethereum.evmcontract import EVMContract
+from mythril.exceptions import UnsatError
 from mythril.laser.ethereum import svm
+from mythril.laser.ethereum.cfg import Node, Constraints
+from mythril.support.model import get_model
+from typing import Dict, Optional, Set, List, Text
 
 from sc3a.analysis.base import AnalysisStrategy
 
@@ -19,33 +23,80 @@ class SingleOwnerStrategy(AnalysisStrategy):
         storage_addresses = self._analyze(laser.nodes)
         return list(storage_addresses)
 
-    def _analyze(self, nodes: dict) -> Optional[Set[Text]]:
+    def _is_unsat(self, proposition: Constraints):
+        """
+        Checks if the *proposition* is unsatisfiable.
+        Parameters
+        ----------
+        proposition: Constraints
+            The proposition to check.
+        Returns
+        -------
+        True if the proposition is unsat, False otherwise.
+        """
+        try:
+            model = get_model(proposition)
+            log.debug('Violation found...')
+            log.debug('### BEGIN DECLARATIONS ###')
+            for d in model.decls():
+                log.debug("<DECL %s = %s>",
+                          d.name(),
+                          re.sub(r'\s{2,}', ' ', str(model[d]).replace('\n', ' ')))
+            log.debug('### END DECLARATIONS ###')
+            return False
+        except UnsatError:
+            return True
+
+    def _analyze(self, nodes: Dict[int, Node]) -> Optional[Set[Text]]:
         log.info('Analyzing nodes of symbolic execution')
         potential_storage_addresses = set()
-        for i, node in nodes.items():
-            saddr = None
-            contains_caller = False
-            for state in node.states:
+        for nidx, node in nodes.items():
+            caller = None
+            storage_item = None
+            storage_address = None
+            for sidx, state in enumerate(node.states):
                 if state.instruction['opcode'] == 'CALLER':
-                    # CALLER has been called, which pushes the message sender's address
-                    # on the stack.
-                    log.debug('Found CALLER instruction in node: %s', i)
-                    contains_caller = True
-                if state.instruction['opcode'] == 'SLOAD':
-                    # We temporarily memorize the storage lookup address for later.
-                    log.debug('Found SLOAD instruction in node: %i', i)
-                    saddr = state.mstate.stack[-1]
-                if state.instruction['opcode'] == 'EQ':
-                    # The program checks if the top two words on the stack are equal, after
-                    # loading something from storage as well as the message sender.
-                    # This means that it's highly likely that the program currently checks
-                    # if the message sender is authorized to perform an operation.
-                    # We store the previously temporarily memorized saddr to a collection of
-                    # possible address spaces, where the "owner" of the contract might be stored.
-                    log.debug('Found EQ instruction in node: %i', i)
-                    if contains_caller and saddr is not None:
-                        log.debug('Found potential storage address: %s', saddr)
-                        potential_storage_addresses.add(saddr)
-        log.info('Found %i potential storage addresses that might \
-                  contain administrator accounts', len(potential_storage_addresses))
+                    # We store the caller item, which sits at the head of the stack
+                    # in the next state in case it is a symbol.
+                    caller = node.states[sidx + 1].mstate.stack[-1]
+                    if caller.symbolic:
+                        log.debug('Found CALLER symbol "%s" in <node=%i, state=%i>',
+                                  caller, nidx, sidx)
+                    else:
+                        caller = None
+                elif state.instruction['opcode'] == 'SLOAD':
+                    # We store the storage address from the current stack and the storage
+                    # item from the next stack in case the latter is a symbol.
+                    storage_item = node.states[sidx + 1].mstate.stack[-1]
+                    if storage_item.symbolic:
+                        log.debug('Found SLOAD instruction in <node=%i, state=%i>',
+                                  nidx, sidx)
+                        storage_address = state.mstate.stack[-1]
+                    else:
+                        storage_item = None
+                elif state.instruction['opcode'] == 'EQ':
+                    # The top two items in the stack are being compared. We first extract
+                    # the two items into stack_0 and stack_1, and check if both are symbols.
+                    # Next, we check if the two items are always equal to the two items that
+                    # we have previously stored. If that is the case, we keep the storage
+                    # address that we got from the SLOAD operation.
+                    log.debug('Found EQ instruction in <node=%i, state=%i>',
+                              nidx, sidx)
+                    stack_0 = state.mstate.stack[-1]
+                    stack_1 = state.mstate.stack[-2]
+                    if not stack_0.symbolic or not stack_1.symbolic:
+                        continue
+                    # Head is the caller and second element is the storage item
+                    proposition_0 = node.constraints
+                    proposition_0.append(stack_0 != caller)
+                    proposition_0.append(stack_1 != storage_item)
+                    # Head is the storage item and second element is the caller
+                    proposition_1 = node.constraints
+                    proposition_1.append(stack_0 != storage_item)
+                    proposition_1.append(stack_1 != caller)
+                    # If either proposition is unsat we keep the storage address
+                    if self._is_unsat(proposition_0) or self._is_unsat(proposition_1):
+                        potential_storage_addresses.add(storage_address)
+        log.info(('Found %i potential storage addresses that might '
+                  'contain administrator accounts'), len(potential_storage_addresses))
         return potential_storage_addresses
