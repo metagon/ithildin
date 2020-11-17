e882cb705ca1c8bd2edca619b5935b1f0627266f
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-11-17 12:12:02 +0100

    Line width formatting

--- sc3a/__init__.py
+++ sc3a/__init__.py
@@ -1,20 +1,13 @@
 import logging
 from logging import handlers
 
-formatter = logging.Formatter((
-    '%(asctime)s '
-    '- %(name)s '
-    '[%(levelname)s\t] '
-    '%(message)s'
-))
+formatter = logging.Formatter('%(asctime)s - %(name)s [%(levelname)s\t] %(message)s')
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
 
-file_handler = handlers.RotatingFileHandler('logs/out.log',
-                                            maxBytes=100_000,
-                                            backupCount=5)
+file_handler = handlers.RotatingFileHandler('logs/out.log', maxBytes=100_000, backupCount=5)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 
