a6998444d4d131f722388778a22fdba86690b1e5
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-11-21 12:17:46 +0100

    Fixed issue where mythril home directory isn't present if mythril program hasn't been executed before #1

--- sc3a/__init__.py
+++ sc3a/__init__.py
@@ -1,6 +1,8 @@
 import logging
 from logging import handlers
 
+from .setup import setup
+
 formatter = logging.Formatter('%(asctime)s - %(name)s [%(levelname)s\t] %(message)s')
 
 stream_handler = logging.StreamHandler()
@@ -15,3 +17,6 @@ log = logging.getLogger(__name__)
 log.addHandler(stream_handler)
 log.addHandler(file_handler)
 log.setLevel(logging.INFO)
+
+# Execute setup script
+setup()
---
+++ sc3a/setup.py
@@ -0,0 +1,16 @@
+import os
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+def setup():
+    # Ensure the mythril home directory '~/.mythril' is present.
+    # This is an issue that arises when mythril is used as a library and tries to acces ~/.mythril/signatures.db
+    # Related issue: https://github.com/metagon/sc3a/issues/1
+    myth_home = os.path.join(os.path.expanduser('~'), '.mythril')
+    if os.path.exists(myth_home):
+        log.debug('Mythril home directory exists')
+    else:
+        log.info('Creating mythril home directory ~/.mythril')
+        os.mkdir(myth_home)
