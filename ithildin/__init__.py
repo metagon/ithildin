58ea0850a61c4dd1ce02cbcdbdb2b08e694e26d0
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-12-22 13:51:53 +0100

    Removed temporary logs directory and logging file_handler

--- ithildin/__init__.py
+++ ithildin/__init__.py
@@ -9,13 +9,8 @@ stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.DEBUG)
 stream_handler.setFormatter(formatter)
 
-file_handler = handlers.RotatingFileHandler('logs/out.log', maxBytes=100_000, backupCount=5)
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-
 log = logging.getLogger(__name__)
 log.addHandler(stream_handler)
-log.addHandler(file_handler)
 log.setLevel(logging.INFO)
 
 # Execute setup script
