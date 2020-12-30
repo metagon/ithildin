9e521460f14cb5dc8391aa7a59e7f1a5543da937
Philippos Gorgoris <philipgorgoris@gmail.com>
2020-12-30 16:56:14 +0100

    Version increment to v0.1.1

--- ithildin/__init__.py
+++ ithildin/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from mythril.mythril.mythril_config import MythrilConfig
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 formatter = logging.Formatter('[%(levelname)s\t] %(asctime)s - %(name)s %(message)s')
 
